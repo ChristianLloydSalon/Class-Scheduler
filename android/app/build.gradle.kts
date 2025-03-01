f91a205b9aa72c5a4eb817b814a6c9b444ed58ca
ChristianLloydSalon <christianlloyd.b.salon@gmail.com>
2025-03-01 09:53:22 +0800

    Initial commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,61 @@
+import java.util.Properties
+import java.io.FileInputStream
+import java.io.File
+
+plugins {
+    id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+// Get the keystore properties
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties().apply { load(FileInputStream(keystorePropertiesFile)) }
+
+android {
+    namespace = "com.classroom.scheduler"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions { jvmTarget = JavaVersion.VERSION_11.toString() }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID
+        // (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.classroom.scheduler"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["releaseStoreFile"].toString())
+            storePassword = keystoreProperties["releaseStorePassword"].toString()
+            keyAlias = keystoreProperties["releaseKeyAlias"].toString()
+            keyPassword = keystoreProperties["releaseKeyPassword"].toString()
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+flutter { source = "../.." }
